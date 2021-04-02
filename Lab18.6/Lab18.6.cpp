d67afea6d38bd8515f9df17ab924a1d5731b9f02
Larra Padla <artem1978354@gmail.com>
2021-04-02 18:12:07 +0500

    Добавьте файлы проекта.

---
+++ Lab18.6/Lab18.6.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "container.h"
+using namespace std;
+int main()
+{
+    container A(5);
+    cout << A;
+    cin >> A;
+    cout << A;
+}
+
---
+++ Lab18.6/container.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class iter
+{
+	friend class container;
+public:
+	iter() { lin = 0; }
+	iter(const iter& it) { lin = it.lin; }
+	bool operator ==(const iter& it) { return lin == it.lin; }
+	bool operator !=(const iter& it) { return !(lin == it.lin); }
+	void operator ++() { ++lin; }
+	void operator --() { --lin; }
+	int& operator *() const { return *lin; }
+	iter& operator + (const int n)
+	{
+		lin += n;
+		return *this;
+	}
+private:
+	int* lin;
+};
+class container
+{
+public:
+	container(int size);
+	container(const container& obj);
+	~container();
+	container& operator = (const container& obj);
+	int& operator [] (const int index);
+	friend ostream& operator <<(ostream& out, const container& other);
+	friend istream& operator >>(istream& out, const container& other);
+	bool operator != (const container& obj);
+	bool operator < (const int value);
+	iter first() { return beg; }
+	iter last() { return end; }
+private:
+	int size;
+	int* data;
+	iter beg;
+	iter end;
+};
+
